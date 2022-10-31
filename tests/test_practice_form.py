56dda75bea788a5aafc284a8cb5ad8895eda552c
Vladislav <djefry.kamen@gmail.com>
2022-10-31 20:44:34 +0400

    fix google ads have size and swap #submit click() to js.click
    add gitignore

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -10,11 +10,10 @@ def test_path(relative_path):
 
 def test_submit_student_registration_form():
     browser.open('https://demoqa.com/automation-practice-form')
-    browser.all("[id^=google_ads][id$=container__]").should(have.size_greater_than_or_equal(3))
+    browser.all("[id^=google_ads][id$=container__]").should(have.size_less_than_or_equal(3))
     browser.execute_script(
         'document.querySelectorAll("[id^=google_ads][id$=container__]").forEach(element => element.remove())'
     )
-    browser.execute_script('document.querySelector("footer").remove()')
     browser.element('#firstName').type('Vladislav')
     browser.element('#lastName').type('Kamenskiy')
     browser.element('#userEmail').type('dje.fry@mail.ru')
@@ -34,4 +33,4 @@ def test_submit_student_registration_form():
     browser.element('#react-select-3-option-1').click()
     browser.element('[id="city"] [class~="css-19bqh2r"]').click()
     browser.element('#react-select-4-option-1').click()
-    browser.element('#submit').click()
+    browser.element('#submit').perform(command.js.click)
